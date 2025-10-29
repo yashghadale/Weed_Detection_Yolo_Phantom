cdbb015afb912f85cc5b42c388b08aa5dce8f479
UltralyticsAssistant <web@ultralytics.com>
2025-10-29 17:35:41 +0000

    Auto-format by https://ultralytics.com/actions

--- ultralytics/nn/modules/block.py
+++ ultralytics/nn/modules/block.py
@@ -2029,4 +2029,3 @@ class SAVPE(nn.Module):
         aggregated = score.transpose(-2, -3) @ x.reshape(B, self.c, C // self.c, -1).transpose(-1, -2)
 
         return F.normalize(aggregated.transpose(-2, -3).reshape(B, Q, -1), dim=-1, p=2)
-
